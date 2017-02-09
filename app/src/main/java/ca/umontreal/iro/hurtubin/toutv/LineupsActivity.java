248cd1cc3d490d849516fa33b14b7dc78a732c0e
Nicolas Hurtubise <316k@legtux.org>
2017-02-09 12:29:52 -0500

    Adds AsyncTask and adapter for the ListView

--- app/src/main/java/ca/umontreal/iro/hurtubin/toutv/LineupsActivity.java
+++ app/src/main/java/ca/umontreal/iro/hurtubin/toutv/LineupsActivity.java
@@ -14,11 +14,12 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
 public class LineupsActivity extends AppCompatActivity {
-    private String type = "Émissions";
+    private String type = "Documentaires";
 
     ListView list;
 
@@ -28,5 +29,70 @@ public class LineupsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lineups);
 
         list = (ListView) findViewById(R.id.lineups);
+
+        LineupFetcher fetcher = new LineupFetcher();
+
+        fetcher.execute();
+    }
+
+    public class LineupFetcher extends AsyncTask<Object, Object, Lineup[]> {
+
+        @Override
+        protected Lineup[] doInBackground(Object... params) {
+
+            Lineup[] lineups = new Lineup[0];
+
+            try {
+                lineups = TouTV.getLineups(type);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return lineups;
+        }
+
+        @Override
+        protected void onPostExecute(final Lineup[] lineups) {
+
+            list.setAdapter(new BaseAdapter() {
+                @Override
+                public int getCount() {
+                    return lineups.length;
+                }
+
+                @Override
+                public Object getItem(int position) {
+                    return null;
+                }
+
+                @Override
+                public long getItemId(int position) {
+                    return 0;
+                }
+
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+
+                    if(convertView == null)
+                        convertView = getLayoutInflater().inflate(R.layout.lineup_view, parent, false);
+
+                    TextView title = (TextView) convertView.findViewById(R.id.title);
+                    TextView description = (TextView) convertView.findViewById(R.id.description);
+                    ImageView image = (ImageView) convertView.findViewById(R.id.image);
+
+                    title.setText(lineups[position].title);
+                    description.setText(lineups[position].shortDescription);
+
+                    Picasso.with(getApplicationContext())
+                            .load(lineups[position].image)
+                            .into(image);
+
+                    return convertView;
+                }
+            });
+
+        }
     }
 }
