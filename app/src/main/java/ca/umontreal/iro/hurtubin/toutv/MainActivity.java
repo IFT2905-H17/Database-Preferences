fb0ae03fa685e0c4e65dc79fef53a4b44cc5aac7
Nicolas Hurtubise <316k@legtux.org>
2017-02-16 13:02:35 -0500

    Adds menu

--- app/src/main/java/ca/umontreal/iro/hurtubin/toutv/MainActivity.java
+++ app/src/main/java/ca/umontreal/iro/hurtubin/toutv/MainActivity.java
@@ -3,8 +3,11 @@ package ca.umontreal.iro.hurtubin.toutv;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -61,5 +64,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        MenuItem about = menu.findItem(R.id.about);
+
+        about.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                Toast.makeText(MainActivity.this, "Cette application a été créée pour des fins pédagogies seulement. Veuillez l'utiliser pour des fins pédagogiques seulement.", Toast.LENGTH_SHORT).show();
+
+                return true;
+            }
+        });
+
+        return true;
     }
 }
